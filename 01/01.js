c35a0698ae7dc8f0ce87623c26e0c850e6c76e57
Natalia Garea <natgarea@gmail.com>
2021-04-28 21:46:25 +0200

    Add exercise 1

---
+++ 01/01.js
@@ -0,0 +1,7 @@
+const app = new Vue({
+    el: '#app', // el contenido de app va a ser gestionado con vue
+    data: {
+        title: 'Hola Mundo con Vue',
+        fruits: ['manzana', 'pera', 'platano']
+    }
+})
