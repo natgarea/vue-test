6eb64df08f970ae2bbdffe3024f01a194aab0be2
Natalia Garea <natgarea@gmail.com>
2021-05-02 11:00:49 +0200

    Add exercise 4 - css classes

---
+++ 04/04.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+    el: '#app', // el contenido de app va a ser gestionado con vue
+    data: {
+        fondo: 'bg-warning',
+        color: false
+    },
+    methods: {
+    }
+})
